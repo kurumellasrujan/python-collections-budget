22c8cab890b76e5edc38aad15a40e3f177272c80
kurumellasrujan <kurumella.srujan@gmail.com>
2023-02-28 15:51:04 -0700

    Module Completed.

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -18,5 +18,5 @@ categories, count = zip(*top5)
 
 fig, ax = plt.subplots()
 ax.bar(categories, count)
-ax.set_title('# of purchases by Category')
+ax.set_title('# of Purchases by Category')
 plt.show()
