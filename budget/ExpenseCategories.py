4805991cceafe8d9b3cf05373780db6f64171f5a
kurumellasrujan <kurumella.srujan@gmail.com>
2023-03-02 09:30:32 -0700

    Completed Module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -10,7 +10,7 @@ def main():
     if not divided_set_comp == divided_for_loop:
         print('Sets are NOT equal by == test')
 
-    for a,b in zip(divided_for_loop, divided_set_comp):
+    for a, b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print('Sets are NOT equal by subtest test')
 
